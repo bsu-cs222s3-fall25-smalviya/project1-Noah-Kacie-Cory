bdcb93a3bfb6bb72eb679731d3fdf7d694b88a68
Cory <sonny11704@gmail.com>
2025-09-23 00:01:38 -0400

    updated formatter to allow errors to show in output

--- Project1-kacie-noah-cory/src/main/java/edu/bsu/cs/Format.java
+++ Project1-kacie-noah-cory/src/main/java/edu/bsu/cs/Format.java
@@ -10,17 +10,29 @@ public class Format {
         JSONObject query = root.getJSONObject("query");
         JSONObject pages = query.getJSONObject("pages");
 
-        // Get first page object
         String firstKey = pages.keys().next();
         JSONObject page = pages.getJSONObject(firstKey);
+
+        if (!page.has("revisions")) {
+           System.out.println("No revisions available.");
+           return;
+        }
         JSONArray revisions = page.getJSONArray("revisions");
 
-        // Print each revision
         for (int i = 0; i < revisions.length(); i++) {
             JSONObject rev = revisions.getJSONObject(i);
             String timestamp = rev.getString("timestamp");
             String user = rev.getString("user");
             System.out.printf("%d  %s  %s%n", i + 1, timestamp, user);
         }
+    }
+
+    public static boolean hasNoPage(String rawJsonData) {
+        JSONObject root = new JSONObject(rawJsonData);
+        JSONObject query = root.getJSONObject("query");
+        JSONObject pages = query.getJSONObject("pages");
+
+        String firstKey = pages.keys().next();
+        return firstKey.equals("-1");
     }
 }
