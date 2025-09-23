e4610bb2b7e8ac288bb70817b4d2062312293f8a
KacieInscoe <kacie.inscoe@bsu.edu>
2025-09-22 22:54:22 -0400

    Update Format.java
    changes the output of the code to look nicer

--- Project1-kacie-noah-cory/src/main/java/edu/bsu/cs/Format.java
+++ Project1-kacie-noah-cory/src/main/java/edu/bsu/cs/Format.java
@@ -2,7 +2,22 @@ package edu.bsu.cs;
 
 public class Format {
 
-    public static void printRawJson(String rawJsonData) {
-        System.out.println(rawJsonData);
+   public static void printFormattedJson(String rawJsonData) {
+        JSONObject root = new JSONObject(rawJsonData);
+        JSONObject query = root.getJSONObject("query");
+        JSONObject pages = query.getJSONObject("pages");
+
+        // Get first page object
+        String firstKey = pages.keys().next();
+        JSONObject page = pages.getJSONObject(firstKey);
+        JSONArray revisions = page.getJSONArray("revisions");
+
+        // Print each revision
+        for (int i = 0; i < revisions.length(); i++) {
+            JSONObject rev = revisions.getJSONObject(i);
+            String timestamp = rev.getString("timestamp");
+            String user = rev.getString("user");
+            System.out.printf("%d  %s  %s%n", i + 1, timestamp, user);
+        }
     }
 }
