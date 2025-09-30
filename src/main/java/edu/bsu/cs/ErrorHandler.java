3fe62cbdef6e96e04458cb6f56bb5565fc4f6cd2
KacieInscoe <kacie.inscoe@bsu.edu>
2025-09-30 13:31:51 -0400

    Update ErrorHandler.java
    adding a class to handle errors for the gui

--- src/main/java/edu/bsu/cs/ErrorHandler.java
+++ src/main/java/edu/bsu/cs/ErrorHandler.java
@@ -1,4 +1,22 @@
 package edu.bsu.cs;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 public class ErrorHandler {
+
+    public static void showError(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, message, ButtonType.OK);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.showAndWait();
+    }
+
+    public static void showInfo(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, message, ButtonType.OK);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.showAndWait();
+    }
+
 }
